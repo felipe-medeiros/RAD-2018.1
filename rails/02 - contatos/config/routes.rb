1bea6505623a690cfdbb41c01bf705762a94cc2e
Valeria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2018-04-12 20:09:42 +0000

    Add 12/04/2018

---
+++ rails/02 - contatos/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  root 'home#index'
+
+  get 'pessoas' => 'people#index'
+
+  resources :emails, only: [:index, :show]
+  resources :people, except: [:index, :show]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
